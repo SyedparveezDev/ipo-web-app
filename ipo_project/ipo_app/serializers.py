4e7a97d9df768ae69284238c694ab71b0f0a3c49
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-14 12:06:19 +0530

    Implement IPOListAPIView and IPOSerializer for IPO management

---
+++ ipo_project/ipo_app/serializers.py
@@ -0,0 +1,9 @@
+# ipo_app/serializers.py
+
+from rest_framework import serializers
+from .models import IPO
+
+class IPOSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IPO
+        fields = '__all__'
--- ipo_project/ipo_app/views.py
+++ ipo_project/ipo_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import IPO
+from .serializers import IPOSerializer
+
+class IPOListAPIView(APIView):
+    def get(self, request):
+        ipos = IPO.objects.all()
+        serializer = IPOSerializer(ipos, many=True)
+        return Response(serializer.data)
