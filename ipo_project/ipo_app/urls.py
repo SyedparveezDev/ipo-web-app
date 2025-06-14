29cf21518095364e882282ffacbdf9874b29979f
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-14 12:12:28 +0530

    Refactor URL routing to include admin and app-specific paths

--- ipo_project/ipo_app/urls.py
+++ ipo_project/ipo_app/urls.py
@@ -3,6 +3,14 @@
 from django.urls import path
 from .views import IPOListAPIView
 
+# ipo_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('api/ipo/', IPOListAPIView.as_view(), name='ipo-list'),
+    path('admin/', admin.site.urls),
+    path('', include('ipo_app.urls')), 
 ]
+
+
