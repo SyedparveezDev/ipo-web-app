f4c576fb9034e22cf397da6ff4495425770c1491
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-14 12:38:40 +0530

    Remove ipo_app and related files from the project

--- ipo_project/ipo_app/serializers.py
+++
@@ -1,9 +0,0 @@
-# ipo_app/serializers.py
-
-from rest_framework import serializers
-from .models import IPO
-
-class IPOSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IPO
-        fields = '__all__'
