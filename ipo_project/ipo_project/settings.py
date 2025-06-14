a31189b3c24da70155966665a25a7ccab91c9902
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-14 10:03:08 +0530

    Configure PostgreSQL database settings in settings.py

--- ipo_project/ipo_project/settings.py
+++ ipo_project/ipo_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'ipo_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ipo_db',
         'USER': 'postgres',
