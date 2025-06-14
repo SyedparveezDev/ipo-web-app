3eb41e2f6c116fb811176ce4d119e7062a0e6573
SyedparveezDev <syedparveezdevlop@gmail.com>
2025-06-14 20:05:03 +0530

    Implement IPO API views and serializer for listing and detail retrieval

--- ipo-web-app/ipo_app/views.py
+++ ipo-web-app/ipo_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import IPO
+from .serializers import IPOSerializer
 
-# Create your views here.
+class IPOListAPIView(APIView):
+    def get(self, request):
+        ipos = IPO.objects.all()
+        serializer = IPOSerializer(ipos, many=True)
+        return Response(serializer.data)
+
+class IPODetailAPIView(APIView):
+    def get(self, request, pk):
+        try:
+            ipo = IPO.objects.get(pk=pk)
+            serializer = IPOSerializer(ipo)
+            return Response(serializer.data)
+        except IPO.DoesNotExist:
+            return Response({"error": "IPO not found"}, status=status.HTTP_404_NOT_FOUND)
+
